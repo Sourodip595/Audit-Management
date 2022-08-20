e707069e6eadec3769776104fb48a91e34770ea5
Sourodip595 <koliasourodip62@gmail.com>
2022-08-21 03:18:19 +0530

    yaml added

--- Audit-Management-App/src/app/Models/Microservices.ts
+++ Audit-Management-App/src/app/Models/Microservices.ts
@@ -2,7 +2,7 @@ export const Microservices : {[key: string]: string} = {
     // give like this : 
     // "ms-name" : "http://localhost:[port-no.]/[context-root]"
     "auth"      : "http://localhost:8100/api/authorization/auth",
-    "checklist" : "http://localhost:8200/api/benchmark1/checklist",
-    "benchmark" : "http://localhost:8250/benchmark",
+    "checklist" : "http://localhost:8200/checklist",
+    "benchmark" : "http://localhost:7000/api/benchmark1/benchmark",
     "severity"  : "http://localhost:8300/severity"
 }
